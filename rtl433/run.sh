246f3d536064253406ab002ff52fc8ee479608fd
Josh Porter <joshporter1@gmail.com>
2021-08-26 17:36:26 -0400

    Add basic single-use rtl_433 Docker container

---
+++ rtl433/run.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/with-contenv bashio
+
+rtl_433 -M utc -R 40 -R 97 -C customary -F "mqtt://core-mosquitto:1883,user=homeassistant,pass=OuGhi1fei9phi4quaiqueenga9ouk2cuDaeb0zee2ohY3thaeth7eJaeghifoh1i,retain=0"
+
+  # host: core-mosquitto
+  # port: 1883
+  # topic_prefix: homeassistant
+  # user: mqtt
+  # password: c3ntr!fug3~
